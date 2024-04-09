1091ff02f6e870898c31794b0356aece3d22c981
Vincent Howard <vincenguyenhoward@gmail.com>
2024-04-08 21:54:57 -0600

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "@/lib/Providers";
 import { NavBar } from "@/components/NavBar/NavBar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <Providers>
           <NavBar />
           {children}
